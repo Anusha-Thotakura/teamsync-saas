5825e122cb9d5567af48a72bedf6a4c28d67a7ae
Anusha Thotakura <anushathotakura666@gmail.com>
2026-02-17 18:25:43 +0530

    Day 1:Initial project setup

---
+++ backend/teamsync-backend/src/test/java/com/teamsync/teamsync_backend/TeamsyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamsync.teamsync_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamsyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
